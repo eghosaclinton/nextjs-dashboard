12e8a10bff871b49f725932f85e98ad765569934
clintt-09 <aisosaclinton09@gmail.com>
2025-01-10 01:29:48 +0100

    change: some cleanup

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,30 +1,27 @@
-import { Metadata } from 'next';
-import { lusitana } from '@/app/ui/font';
+import { Metadata } from 'next'
+import CustomersTable from '@/app/ui/customers/table'
+import { Suspense } from 'react'
+import { fetchFilteredCustomers } from '@/app/lib/data'
 
 export const metadata: Metadata = {
     title: 'Customers',
-  };
+}
 
-
-export default function Page() {
-    return(
-            <div className="w-full">
-                    <div className="flex w-full items-center justify-between">
-                        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
-                    </div>
-                    {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                        <Search placeholder="Search invoices..." />
-                        <CreateInvoice />
-                    </div> */}
-                    {/* <Suspense
-                        key={query + currentPage}
-                        fallback={<InvoicesTableSkeleton />}
-                    >
-                        <Table query={query} currentPage={currentPage} />
-                    </Suspense>
-                    <div className="mt-5 flex w-full justify-center">
+export default async function Page(props: {
+    searchParams?: Promise<{ query?: string; page?: string }>
+}) {
+    const searchParams = await props.searchParams
+    const query = searchParams?.query || ''
+    // const currentPage = Number(searchParams?.page) || 1
+    const customers = await fetchFilteredCustomers(query)
+    return (
+        <div className="w-full">
+            <Suspense fallback={<h1>Loading</h1>}>
+                <CustomersTable customers={customers} />
+            </Suspense>
+            {/* <div className="mt-5 flex w-full justify-center">
                         <Pagination totalPages={totalPages} />
                     </div> */}
-                </div>
+        </div>
     )
 }
